9f958e52478c1c8b469ceb0d2b35f7f0478963aa
fraserxu <xvfeng123@gmail.com>
2015-02-09 20:18:55 +0800

    rewrite, save my pg

---
+++ src/components/City.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import api from '../utils/api';
+
+const City = React.createClass({
+
+  displayName: 'City',
+
+  getInitialState() {
+    return {
+      data: null
+    };
+  },
+
+  componentWillMount() {
+    this.fetchData()
+  },
+
+  fetchData() {
+    api.getWeatherByCityID(this.props.id, (data) => {
+      api.fetchCityData(this.props.name, {}, (sensorData) => {
+        this.setState({
+          data: data,
+          sensorData: sensorData
+        })
+      })
+
+    })
+  },
+
+  render() {
+    var icon
+    if (this.state.data) icon = `http://openweathermap.org/img/w/${this.state.data.weather[0].icon}.png`
+
+    return (
+      <section className='city'>
+        {this.props.name}
+        <div className='weather'>
+          <img src={icon} />
+        </div>
+      </section>
+    );
+  }
+
+});
+
+export default City;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,10 @@
 var d3 = require('d3')
+var qs = require('querystring')
+var moment = require('moment')
+var dateUtils = require('./dateUtils')
+
+var cities = require('../data/cities.json')
+
 var SOURCE = 'ci4s0caqw000002wey2s695ph'
 var API_URL = 'http://sensor-api.localdata.com/api/v1'
 var OSM_URL = 'http://nominatim.openstreetmap.org/reverse'
@@ -15,9 +21,27 @@ var API = {
       API.reverseGEOCode(lat, lng, (address) => {
         // save location to the first item
         res.data[0].address = address
-        console.log('res', res)
         cb(null, res.data)
       })
+    })
+  },
+
+  fetchCityData(city, params, cb) {
+    if (!city) throw new Error('city name is required')
+
+    var ops = {
+      fields: params.fields || 'temperature,light,airquality_raw,sound,humidity,dust',
+      from: params.from || dateUtils.toISO(dateUtils.getPreviousDay(Date.now(), 24)),
+      before: params.to || dateUtils.toISO(Date.now()),
+      resolution: params.resolution || '1h',
+      'over.city': city
+    }
+
+    var url = `http://sensor-api.localdata.com/api/v1/aggregations?${qs.stringify(ops)}`
+
+    d3.json(url, (res) => {
+      console.log('res', res)
+      cb(res)
     })
   },
 
@@ -25,7 +49,21 @@ var API = {
     d3.json(`${OSM_URL}?format=json&lat=${lat}&lon=${lng}&zoom=18&addressdetails=1`, (data) => {
       cb(data)
     })
+  },
+
+  getByCityName(name, cb) {
+    var cityID = cities[name]
+    if (!cityID) throw new Error('city not found')
+    API.getWeatherByCityID(cityID, cb)
+  },
+
+  getWeatherByCityID(id, cb) {
+    d3.json(`http://api.openweathermap.org/data/2.5/weather?id=${id}`, (data) => {
+      cb(data)
+    })
   }
 }
+
+window.API = API
 
 module.exports = API;
