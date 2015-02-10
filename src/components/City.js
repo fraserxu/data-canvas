8c1f77eb979feb4beb9f128aab3e5a4ded1b2ebd
fraserxu <xvfeng123@gmail.com>
2015-02-11 00:35:46 +0800

    display data from sensor

--- src/components/City.js
+++ src/components/City.js
@@ -34,12 +34,19 @@ const City = React.createClass({
   },
 
   render() {
-    var icon, temperature
+    var icon, temperature, aqi, dust, humidity, noise, light
     if (this.state.data) icon = `http://openweathermap.org/img/w/${this.state.data.weather[0].icon}.png`
-    if (this.state.sensorData) temperature = parseFloat(this.state.sensorData.data[this.state.sensorData.data.length - 1]['temperature']).toFixed(2)
+    if (this.state.sensorData) {
+      var latest = this.state.sensorData.data[this.state.sensorData.data.length - 1]
+      temperature = parseFloat(latest['temperature']).toFixed(2)
+      aqi = parseFloat(latest['airquality_raw']).toFixed(2)
+      dust = parseFloat(latest['dust']).toFixed(2)
+      humidity = parseFloat(latest['humidity']).toFixed(2)
+      noise = parseFloat(latest['sound']).toFixed(2)
+      light = parseFloat(latest['light']).toFixed(2)
+    }
     var headerStyle = {
-      backgroundImage: `url(${this.state.flickUrl})`,
-      opacity: '0.8'
+      backgroundImage: `url(${this.state.flickUrl})`
     }
 
     return (
@@ -51,6 +58,21 @@ const City = React.createClass({
             <img src={icon} />
           </div>
         </header>
+        <section className='indicator aqi'>
+          AQI: {aqi} mV
+        </section>
+        <section className='indicator dust'>
+          Dust: {dust} pcs/238mL
+        </section>
+        <section className='indicator humidity'>
+          Humidity: {humidity} %
+        </section>
+        <section className='indicator noise'>
+          Noise: {noise} mV
+        </section>
+        <section className='indicator light'>
+          Light: {light} Lux
+        </section>
       </section>
     );
   }
